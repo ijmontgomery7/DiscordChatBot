4704cc76d4553824263c501916c1a05543fdfd51
Ian Montgomery <ijm4219@g.rit.edu>
2016-11-13 12:06:50 -0500

    added some documentation to make the code easier to understand

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 /**
- * Created by Ian on 11/10/2016.
+ * Created by Ian Montgomery (IJMontomery7)
+ *
+ * This simple discord bot allows a user to set messages the bot will reply to
  */
 
 
 var Discord = require("discord.js");
 
 
-var bot = new Discord.Client();
+var bot = new Discord.Client(); // setup required for discord.js
 
 var dictionary = {};
 
@@ -15,7 +17,7 @@ bot.on("message", function (message) {
 
     if (parsedLine[0] == ":set"){
         if (parsedLine.length < 4 ){
-            message.reply("correct format is: :set message -> reply");
+            message.reply("correct format is: :set message -> reply"); // tells how to use the :set command
         }
         else {
             var first = parsedLine[1];
@@ -23,11 +25,11 @@ bot.on("message", function (message) {
             var x = 2;
             var mark = 0;
             while (x < parsedLine.length) {
-                if (parsedLine[x] != "->" && mark == 0) {
+                if (parsedLine[x] != "->" && mark == 0) { // words before -> will be put in first
                     first = first.concat(" ");
                     first = first.concat(parsedLine[x]);
                 }
-                if (parsedLine[x] != "->" && mark == 1) {
+                if (parsedLine[x] != "->" && mark == 1) { // words after -> will be put in second
                     second = second.concat(" ");
                     second = second.concat(parsedLine[x]);
                 }
@@ -38,13 +40,13 @@ bot.on("message", function (message) {
             }
 
 
-            dictionary[first] = second;
+            dictionary[first] = second; // sets the commands into the dictionary
         }
     }
     if(message == ":clear"){
         dictionary = {};
     }
-    else {
+    else { // goes through the list of keys, if the message is the key the bot will respond with the value
         for (var key in dictionary) {
             var value = dictionary[key];
             if (message.content == key) {
@@ -58,5 +60,5 @@ bot.on("message", function (message) {
 
 
 
-
+// Discord.js' way to log the bot on
 bot.login("MjQ2MTQ1MDc3NzQ1OTQyNTMw.CwWX_w.OxCsOdREUf0V9YfPqEOVfLC4QnQ");

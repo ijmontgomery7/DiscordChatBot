bd0bc97f89ea3fb784cc08a9e70b109a1e9fa99e
Ian Montgomery <ijm4219@g.rit.edu>
2016-11-13 11:18:42 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Ian on 11/10/2016.
+ */
+
+
+var Discord = require("discord.js");
+
+
+var bot = new Discord.Client();
+
+var dictionary = {};
+
+bot.on("message", function (message) {
+    if (message.content.)
+})
+
+
+
+
+bot.login("MjQ2MTQ1MDc3NzQ1OTQyNTMw.CwWX_w.OxCsOdREUf0V9YfPqEOVfLC4QnQ");

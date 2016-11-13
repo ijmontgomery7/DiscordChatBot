aca5f8d28ac8af5c5b1548b0ce7ac4a3db2fe764
Ian Montgomery <ijm4219@g.rit.edu>
2016-11-13 11:58:06 -0500

    Bot will now add messages and respond to the message set

--- main.js
+++ main.js
@@ -11,8 +11,49 @@ var bot = new Discord.Client();
 var dictionary = {};
 
 bot.on("message", function (message) {
-    if (message.content.)
-})
+    var parsedLine = message.content.split(" ");
+
+    if (parsedLine[0] == ":set"){
+        if (parsedLine.length < 4 ){
+            message.reply("correct format is: :set message -> reply");
+        }
+        else {
+            var first = parsedLine[1];
+            var second = "";
+            var x = 2;
+            var mark = 0;
+            while (x < parsedLine.length) {
+                if (parsedLine[x] != "->" && mark == 0) {
+                    first = first.concat(" ");
+                    first = first.concat(parsedLine[x]);
+                }
+                if (parsedLine[x] != "->" && mark == 1) {
+                    second = second.concat(" ");
+                    second = second.concat(parsedLine[x]);
+                }
+                else {
+                    mark = 1;
+                }
+                x++;
+            }
+            
+
+            dictionary[first] = second;
+        }
+    }
+    if(parsedLine[0] != ":set") {
+        for (var key in dictionary) {
+            var value = dictionary[key];
+            if (message.content == key) {
+                message.reply(value);
+            }
+        }
+    }
+
+    if(message == ":clear"){
+        dictionary = {};
+    }
+});
 
 
 
